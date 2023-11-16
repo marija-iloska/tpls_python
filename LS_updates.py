ed7540a60ddc5694f7d17f61fe68f1ee3277cc41
marija-iloska <marija.iloska@stonybrook.edu>
2023-11-15 19:01:05 -0500

    formatting

--- LS_updates.py
+++ LS_updates.py
@@ -1,87 +1,82 @@
 import numpy as np
 
+
 # ORLS ascending ====================================
 def orls_ascend(y, H, k, K, m, t, D, theta):
-
     # Current input data (loops 0 to index minus 1)
     Hk = H[:t, :k]
     y_past = y[:t]
 
     # Define the new feature h_new(k+1)
-    h_new = H[:t, k+m]
+    h_new = H[:t, k + m]
 
     # Projection matrix
     DHkT = D @ Hk.T
     P_norm = np.eye(t) - Hk @ DHkT
-
 
     # Some reusable terms
     hnP = h_new @ P_norm
     hnPy = hnP @ y_past
     d = DHkT @ h_new
 
-
     # Compute terms of D(k+1)
     D22 = 1 / (hnP @ h_new)
-    D12 = - d*D22
+    D12 = - d * D22
     D21 = D12.T
-    D11 = D + np.outer(d,d)*D22
+    D11 = D + np.outer(d, d) * D22
 
     # Create D(k+1)
     top = np.vstack([D11, D12])
-    D = np.hstack([top, np.append(D21, D22).reshape(k+1,1)])
+    D = np.hstack([top, np.append(D21, D22).reshape(k + 1, 1)])
 
     # Update theta(k) to theta(k+1)
-    hnPyD = hnPy*D22  #reusable term
-    theta = np.vstack( [theta - hnPyD*d.reshape(k,1), hnPyD])
+    hnPyD = hnPy * D22  # reusable term
+    theta = np.vstack([theta - hnPyD * d.reshape(k, 1), hnPyD])
 
     # Update feature indices
-    idx_H = list(range(0,k)) + [k+m] + list( np.setdiff1d( list(range(k,K)), [k+m] ) )
+    idx_H = list(range(0, k)) + [k + m] + list(np.setdiff1d(list(range(k, K)), [k + m]))
 
     # Update Hk in time and order
-    Hk = H[0:t+1,:][:, idx_H[:k+1]]
-
+    Hk = H[0:t + 1, :][:, idx_H[:k + 1]]
 
     return theta, D, idx_H, Hk
 
 
 # ORLS descending step ====================================
 def orls_descend(H, k, K, m, t, D, theta):
-
     # Range of indices to include
-    idx = list(np.setdiff1d( list(range(0,k)),m ))
+    idx = list(np.setdiff1d(list(range(0, k)), m))
 
     # Update Hk
-    Hk = H[:t+1,:][:, idx + [m]]
+    Hk = H[:t + 1, :][:, idx + [m]]
 
     # Update H
-    idx_H = idx + list(range(k,K)) + [m]
+    idx_H = idx + list(range(k, K)) + [m]
 
     # Get D(k+1) bar
     Dswap = np.empty((k, k))
-    Dswap[0:k-1,:][:,0:k-1] = D[idx, :][:, idx]
-    Dswap[k-1,0:k-1] = D[m, idx]
-    Dswap[:, k-1] = D[ idx+[m], m]
+    Dswap[0:k - 1, :][:, 0:k - 1] = D[idx, :][:, idx]
+    Dswap[k - 1, 0:k - 1] = D[m, idx]
+    Dswap[:, k - 1] = D[idx + [m], m]
 
     # Get D(k+1) bar blocks
-    D11 = Dswap[:k-1,:][:,:k-1]
-    D12 = Dswap[:k-1, k-1:]
+    D11 = Dswap[:k - 1, :][:, :k - 1]
+    D12 = Dswap[:k - 1, k - 1:]
     D21 = D12.T
-    D22 = Dswap[k-1,k-1]
+    D22 = Dswap[k - 1, k - 1]
 
     # Get D(k) final
-    D = D11 - (D12 @ D21)/D22
+    D = D11 - (D12 @ D21) / D22
 
     # Ratio
-    ratio = D12/D22
+    ratio = D12 / D22
 
     # Update rest of theta
-    theta = theta[idx] - theta[m]*ratio.reshape(k-1,1)
+    theta = theta[idx] - theta[m] * ratio.reshape(k - 1, 1)
 
-    idx_k = idx + [m] + list(range(k,K))
+    idx_k = idx + [m] + list(range(k, K))
 
-    return theta, D,  idx_H, Hk
-
+    return theta, D, idx_H, Hk
 
 
 # TRLS update ====================================
@@ -93,20 +88,20 @@ def trls_update(y, hk, theta, D, var_y):
     e = y - hk @ theta
 
     # Find current sigma
-    Sigma = var_y*D
+    Sigma = var_y * D
 
     # Update gain
     temp = Sigma @ hk
     K = temp / (var_y + hk @ temp)
 
     # Update estimate
-    theta = theta + e*K.reshape(k,1)
+    theta = theta + e * K.reshape(k, 1)
 
     # Update covariance
     temp = np.eye(k) - np.outer(K, hk)
     Sigma = np.matmul(temp, Sigma)
 
     # Update D
-    D = Sigma/var_y
+    D = Sigma / var_y
 
     return theta, D
--- jpls.py
+++ jpls.py
@@ -3,38 +3,37 @@ from numpy import linalg as la
 import LS_updates as LS
 import jumps as JP
 
-def JPLS(y, H, t0, var_y):
 
+def JPLS(y, H, t0, var_y):
     # Store true H
     H_true = H
 
     # Get dimensions
-    T = len(H[:,0])
-    K = len(H[0,:])
+    T = len(H[:, 0])
+    K = len(H[0, :])
 
     # Initialize
-    k = round(K/2)
-    Hk = H[list(range(t0+1)),:][:, list(range(k))]
-    Dk = la.inv(Hk[:t0-1,:].T @ Hk[:t0-1,:])
-    theta_k = Dk @ Hk[:t0-1,:].T @ y[:t0-1]
+    k = round(K / 2)
+    Hk = H[list(range(t0 + 1)), :][:, list(range(k))]
+    Dk = la.inv(Hk[:t0 - 1, :].T @ Hk[:t0 - 1, :])
+    theta_k = Dk @ Hk[:t0 - 1, :].T @ y[:t0 - 1]
     idx_H = list(range(k))
 
     # Initialize
-    e = y[t0-1] - Hk[t0-1,:] @ theta_k
-    J = e**2
+    e = y[t0 - 1] - Hk[t0 - 1, :] @ theta_k
+    J = e ** 2
     e_pred = [e]
     J_pred = [J]
     theta_store = []
     idx_store = []
-    J_jump = theta_jump = idx_jump = k_jump = Dk_jump = [0, 0, 0]
-
+    # J_jump = theta_jump = idx_jump = k_jump = Dk_jump = [0, 0, 0]
 
     # Start time loop
-    for t in range(t0,T-1):
+    for t in range(t0, T - 1):
 
         # Update J stay
-        e = y[t] - Hk[t,:] @ theta_k
-        J = float(J + e**2)
+        e = y[t] - Hk[t, :] @ theta_k
+        J = float(J + e ** 2)
 
         # Collect current state of estimate
         stay = [theta_k, list(range(K)), J, Dk, k]
@@ -64,7 +63,7 @@ def JPLS(y, H, t0, var_y):
         theta_jump = [theta_stay, theta_up, theta_down]
 
         # CRITERION to jump
-        minJ = J_jump.index( min(J_jump) )
+        minJ = J_jump.index(min(J_jump))
 
         # Move to chosen model
         H = H[:, idx_jump[minJ]]
@@ -74,23 +73,21 @@ def JPLS(y, H, t0, var_y):
         k = k_jump[minJ]
 
         # Some Quantity updates
-        Hk = H[:t+2, list(range(k))]
+        Hk = H[:t + 2, list(range(k))]
         theta_store.append(theta_k)
 
         # Predictive error
         J_pred.append(J)
-        e_pred.append(y[t] - Hk[t,:] @ theta_k)
+        e_pred.append(y[t] - Hk[t, :] @ theta_k)
 
         # FIND which features were used
         idx_jpls = []
         for i in range(K):
-            if H[0,i] in H_true[0,:]:
-                idx_jpls.append(int( np.where(H_true[0,:] == H[0,i])[0] ))
+            if H[0, i] in H_true[0, :]:
+                idx_jpls.append(int(np.where(H_true[0, :] == H[0, i])[0]))
         idx_store.append(idx_jpls[:k])
 
-
         # TIME UPDATE
-        theta_k, Dk = LS.trls_update(y[t], Hk[t,:k], theta_k, Dk, var_y)
-
+        theta_k, Dk = LS.trls_update(y[t], Hk[t, :k], theta_k, Dk, var_y)
 
     return theta_k, idx_store, theta_store, J_pred
